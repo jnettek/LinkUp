97da368a094792d19dad45ca4d129a6a0adc9b51
jnettek <jnettekpelletier@live.com>
2024-02-04 16:05:43 -0500

    onboarding page

---
+++ app/components/forms/AccountProfile.tsx
@@ -0,0 +1,78 @@
+"use client"
+
+import { z } from "zod"
+import { useForm } from 'react-hook-form';
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { UserValidation } from '@/lib/validations/user';
+
+interface Props {
+    user: {
+        id: string;
+        objectId: string;
+        username: string;
+        name: string;
+        bio: string;
+        image: string;
+    };
+    btnTitle: string;
+}
+
+const AccountProfile = ({user, btnTitle} : Props) => {
+
+     // 1. Define your form.
+    const form = useForm({
+        resolver: zodResolver(UserValidation) ,
+        defaultValues: {
+            profile_photo: '',
+            name: '',
+            username: '',
+            bio: ''
+        }
+    });
+
+        // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof UserValidation>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+      }
+
+    return (
+        <Form {...form}>
+        <form 
+        onSubmit={form.handleSubmit(onSubmit)} 
+        className="flex flex-col justify-start gap-10">
+          <FormField
+            control={form.control}
+            name="profile_photo"
+            render={({ field }) => (
+              <FormItem className="flex items-center gap-4">
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    )
+}
+
+export default AccountProfile;
--- app/components/shared/Topbar.tsx
+++ app/components/shared/Topbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 function Topbar() {
     return (
-        <nav className="fixed top-0 z-30 flex w-full items-center justify-between bg-dark-2 px-6 py-3">
+        <nav className="topbar">
             <Link href="/" className="flex items-center gap-4">
             <Image src="/assets/logo.svg" alt="logo" width={28} height={28}/>
             <p className="text-heading3-bold text-light-1 max-xs:hidden">Link Up</p>
